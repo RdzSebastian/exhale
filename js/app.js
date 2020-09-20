421e570d731df4558d087ae2f55f7bc7bd870c1e
PabloNad <pablonadra@gmail.com>
2020-09-20 17:42:34 -0300

    Se sacan IsNavegador de js y se incluyen las clases mobile y desktop para cambiar objeto dado dispositivo

--- js/app.js
+++ js/app.js
@@ -1,26 +1,5 @@
 var elements = document.querySelectorAll('.text__bottomleft');
 
-// Opera 8.0+
-var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
-
-// Firefox 1.0+
-var isFirefox = typeof InstallTrigger !== 'undefined';
-
-// Safari 3.0+ "[object HTMLElementConstructor]" 
-var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification));
-
-// Internet Explorer 6-11
-var isIE = /*@cc_on!@*/false || !!document.documentMode;
-
-// Edge 20+
-var isEdge = !isIE && !!window.StyleMedia;
-
-// Chrome 1 - 79
-var isChrome = !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
-
-// Edge (based on chromium) detection
-var isEdgeChromium = isChrome && (navigator.userAgent.indexOf("Edg") != -1);
-
 const options = {
     threshold: 0,
     rootMargin: "-50% 0px -15% 0px"
